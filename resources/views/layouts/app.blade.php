718d33145dfcff6653b95fad0ed7b5936c235c1b
julio <juliorzomc@gmail.com>
2025-03-05 14:28:32 -0400

    frist commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'Amazontech') }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="antialiased">
+    <div id="app">
+        <!-- Conteúdo da página -->
+        @yield('content')
+        <!-- Adicionando o Footer -->
+        <x-footer />
+    </div>
+</body>
+</html>
