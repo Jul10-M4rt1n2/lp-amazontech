f95f3d4f3fd6ba5eff701c38c5a5f8d4650eb203
julio <juliorzomc@gmail.com>
2025-03-05 17:13:48 -0400

    finish footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,33 @@
-<footer class="bg-gray-800 text-white py-4">
-    <div class="container mx-auto text-center">
-        <p>&copy; 2025 Your Company. All rights reserved.</p>
+<footer
+    class="footer-content overflow-hidden d-flex flex-column flex-md-row justify-content-between align-items-start">
+    <div class="logo d-flex flex-column align-items-start gap-2 mb-3 mb-md-0">
+        <img class="img-fluid" src="{{ asset('img/logo.svg') }}" alt="logo">
+        <p class="text-copy">&copy; 2025 AmazonTech</p>
+    </div>
+    <div class="nav d-flex flex-column flex-md-row align-items-start gap-3 mb-3 mb-md-0">
+        <div class="footer-group d-flex flex-column justify-content-center align-items-start gap-2">
+            <h3 class="title-nav-footer">
+                Navegação
+            </h3>
+            <ul class="list-items-footer d-flex flex-column justify-content-center align-items-start gap-2">
+                <li><a class="link-item-footer" href="#">Home</a></li>
+                <li><a class="link-item-footer" href="#">Produtos</a></li>
+                <li><a class="link-item-footer" href="#">Sobre</a></li>
+                <li><a class="link-item-footer" href="#">Contato</a></li>
+            </ul>
+        </div>
+        <div class="footer-group d-flex flex-column justify-content-center align-items-start gap-2">
+            <h3 class="title-nav-footer">
+                Contato
+            </h3>
+            <a class="footer-email" href="mailto:amazontech@rr.sebrae.com.br">amazontech@rr.sebrae.com.br</a>
+        </div>
+    </div>
+    <div class="social d-flex justify-content-center justify-content-md-start align-items-center gap-2">
+        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fab fa-youtube"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
     </div>
 </footer>
