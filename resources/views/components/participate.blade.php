80cafcb7e9a6410d0b854a43d7e46391a2a43198
julio <juliorzomc@gmail.com>
2025-03-06 09:05:06 -0400

    finish section participate

---
+++ resources/views/components/participate.blade.php
@@ -0,0 +1,33 @@
+<section class="participate-content overflow-hidden">
+    <div class="container">
+        <div class="row gap-2">
+            <div class="col-lg-8 col-md-12">
+                <div class="participate-content-text d-flex flex-column align-items-start">
+                    <h2 class="title-participe">Quem pode participar</h2>
+                    <p class="text-participe">O Amazontech é um evento aberto a <strong>empreendedores, empresários e profissionais</strong>
+                        que desejam
+                        ampliar, diversificar ou iniciar negócios sustentáveis. Podem participar <strong>empresários de
+                            diferentes setores, empreendedores</strong> urbanos e rurais, <strong>dirigentes</strong> de
+                        entidades empresariais,
+                        <strong>profissionais liberais</strong> e pessoas que buscam complementar a renda, incluindo
+                        <strong>donas de casa</strong>
+                        interessadas em aumentar a renda familiar.
+                    </p>
+                    <p class="text-participe">
+                        Também são bem-vindos <strong>pesquisadores, técnicos, extensionistas, estudantes de cursos
+                            profissionalizantes,</strong> além de <strong>docentes, discentes e dirigentes de
+                            instituições públicas e
+                            privadas ligadas à ciência e tecnologia.</strong> O evento também recebe
+                        <strong>instituições de ensino,
+                            professores e parlamentares,</strong> reunindo diversos atores para fomentar inovação,
+                        conhecimento e
+                        crescimento sustentável.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-12">
+                <div class="participate-content-image">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
