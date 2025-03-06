4fb3f905fa92db12acf60a079287161ef4d841c9
julio <juliorzomc@gmail.com>
2025-03-06 10:21:49 -0400

    finish section realization

---
+++ resources/views/components/realization.blade.php
@@ -0,0 +1,22 @@
+<section class="realization-content overflow-hidden">
+    <div class="container">
+        <div class="content-realization row flex-column justify-content-center align-items-center">
+            <div class="content-logos d-inline-flex flex-column align-items-center">
+                <h2 class="title-realization">Realização</h2>
+                <div class="logos">
+                    <img src="{{ asset('img/image1.png') }}" alt="Logo Amazontech">
+                    <img src="{{ asset('img/logo-sebrae.svg') }}" alt="Logo UFAM">
+                    <img src="{{ asset('img/image3.png') }}" alt="Logo UFAM">
+                    <img src="{{ asset('img/image2.png') }}" alt="Logo UFAM">
+                </div>
+                <div class="patrocinadores-realization d-flex flex-column align-items-center text-center">
+                    <h2 class="title-realization">Leve sua marca para o AmazonTech 2025!</h2>
+                    <p class="text-realization">Entenda como funciona para levar sua marca com o nosso <a
+                            class="link-realization" href="" target="_blank">plano de
+                            patrocínio.</a></p>
+                    <a href="#" class="btn btn-orange-realization" target="_blank">QUERO LEVAR MINHA MARCA</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
