9d2d5f42a326fab25c1a572d681a6efa6b6269c9
Caio Zidane <caiozidane@gmail.com>
2025-03-07 08:46:29 -0400

    icon menu

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,8 @@
 <header class="custom-navbar">
   <a href="#"><img src="{{ asset('img/logo-amazontech-home.svg') }}" class="logo-home" alt="Logo-home"></a>
+  <button class="nav-toggle" aria-label="toggle navigation">
+    <img src="{{ asset('img/menu-fill.svg')}}" class="icon-menu" alt="Icone menu">
+  </button>
   <ul class="nav-links">
       <li><a href="#">O Evento</a></li>
       <li><a href="#">Metodologia</a></li>
