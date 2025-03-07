46934b6df7011e0d97e08a2efb51b1317de00993
julio <juliorzomc@gmail.com>
2025-03-07 09:52:30 -0400

    finish section business

---
+++ resources/views/components/business.blade.php
@@ -0,0 +1,18 @@
+<section class="business-content overflow-hidden">
+    <div class="row">
+        <div class="img-background-business col-lg-4 col-md-12 order-2 order-md-1">
+        </div>
+        <div class="col-lg-8 col-md-12 order-1 order-md-2">
+            <div class="content-busuness-text row flex-column justify-center align-items-center">
+                <h6 class="business-subtitle">REVISTA</h6>
+                <h2 class="business-title">Falando de Negócios</h2>
+                <p class="business-text">Baixe agora, gratuitamente, a <strong>edição especial</strong> da nossa
+                    revista, focada em
+                    sustentabilidade e inovação.</p>
+
+                <a href="https://www.falandodenegociosbr.com.br/_files/ugd/c658da_9680d31f23c84e6ebe4b53ebdca5d929.pdf"
+                    target="_blank" class="btn btn-origin-business">Baixar agora</a>
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<x-business />
 <x-debates />
 <x-participate />
 <x-realization />
