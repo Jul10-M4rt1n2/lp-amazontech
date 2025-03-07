09158235109e5e7a771c8e04905b00cc4e4b6eac
Caio Zidane <caiozidane@gmail.com>
2025-03-07 09:57:40 -0400

    section about event

---
+++ resources/views/components/about-event.blade.php
@@ -0,0 +1,28 @@
+<div class="bg-about-event">
+  <div class="row mx-auto">
+    <div class="content-event">
+      <p class="title-event">
+        Sobre o evento
+      </p>
+      <p class="text-event">
+        O Amazontech é um programa do Sebrae que envolve os <span>estados da Amazônia Legal</span>, universidades, instituições de pesquisa e parceiros estratégicos para <span>promover inovação, tecnologia e negócios sustentáveis na região.</span> 
+      </p>
+      <p class="text-event">
+      O evento tem o compromisso de integrar conhecimento tradicional e científico, fortalecer cadeias produtivas e fomentar a economia verde, com foco no equilíbrio entre desenvolvimento econômico e preservação ambiental.
+      </p>
+      <p class="text-event">
+      Ao longo de mais de duas décadas, o Amazontech consolidou-se como um espaço de articulação e inovação, promovendo soluções que aliam ciência, tecnologia e empreendedorismo para impulsionar um modelo sustentável de crescimento para a Amazônia.
+      </p>
+    </div>
+  </div>
+  <div class="row mx-auto">
+  <div class="content-methodology">
+      <p class="title-methodology">
+        Metodologia
+      </p>
+      <p class="text-methodology">
+      Sua metodologia inclusiva e  participativa  envolve representantes da sociedade, empresários, produtores rurais, pesquisadores, lideranças comunitárias e órgãos governamentais que objetiva criar um ambiente propício para o empreendedorismo e para o desenvolvimento de negócios inovadores e sustentáveis no bioma amazônico tendo como base a geração e aplicação de conhecimento voltado ao desenvolvimento sustentável.
+      </p>
+    </div>
+  </div>
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <x-hero />
+<x-about-event />
 <x-debates />
 <x-participate />
 <x-realization />
