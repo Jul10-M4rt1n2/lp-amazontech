487df84deb3bbb010100f6a3bf1f11390ca908a4
Caio Zidane <caiozidane@gmail.com>
2025-03-07 10:04:20 -0400

    navbar mobile

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <header class="custom-navbar">
   <a href="#"><img src="{{ asset('img/logo-amazontech-home.svg') }}" class="logo-home" alt="Logo-home"></a>
-  <button class="nav-toggle" aria-label="toggle navigation">
+  <button class="nav-toggle d-lg-none" aria-label="toggle navigation">
     <img src="{{ asset('img/menu-fill.svg')}}" class="icon-menu" alt="Icone menu">
   </button>
   <ul class="nav-links">
