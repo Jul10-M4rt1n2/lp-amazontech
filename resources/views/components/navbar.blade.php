1797a68a8a07fe1459d9bbeab0d4c1e1a3f770ac
Caio Zidane <caiozidane@gmail.com>
2025-03-09 22:25:20 -0400

    fix navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,15 +1,29 @@
-<header class="custom-navbar">
-  <a href="#"><img src="{{ asset('img/logo-amazontech-home.svg') }}" class="logo-home" alt="Logo-home"></a>
-  <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+<header class="bg-nav">
+  <nav class="navbar navbar-expand-md mx-lg-3">
+  <a href="#" class="navbar-brand mx-auto">
+    <img src="{{ asset('img/logo-amazontech-home.svg') }}" class="logo-home" alt="Logo-home">
+  </a>
+  <button class="d-lg-none"  
+    type="button" 
+    data-bs-toggle="collapse" 
+    data-bs-target="#navbarNav" 
+    aria-controls="navbarNav" 
+    aria-expanded="false" 
+    aria-label="Toggle navigation">
     <img src="{{ asset('img/menu-fill.svg')}}" class="icon-menu" alt="Icone menu">
   </button>
-  <ul class="nav-links collapse" id="navbarToggleExternalContent">
-      <li><a href="#">O Evento</a></li>
-      <li><a href="#">Metodologia</a></li>
-      <li><a href="#">Eixos de Debate</a></li>
-      <li><a href="#">Quem pode Participar</a></li>
-      <li><a target="_blank" href="https://www.flickr.com/photos/sebraerr/albums/72177720323944909/">Fotos</a></li>
-      <li><a href="#">Notícias</a></li>
-  </ul>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="custom-navbar navbar-nav ms-auto">
+        <li class="">
+          <a class="link-custom" href="#">O Evento</a>
+        </li>
+        <li class=""><a class="link-custom" href="#">Metodologia</a></li>
+        <li class="nav-item"><a class="link-custom" href="#">Eixos de Debate</a></li>
+        <li class="nav-item"><a class="link-custom" href="#">Quem pode Participar</a></li>
+        <li class="nav-item"><a class="link-custom" target="_blank" href="https://www.flickr.com/photos/sebraerr/albums/72177720323944909/">Fotos</a></li>
+        <li class="nav-item"><a class="link-custom" href="#">Notícias</a></li>
+    </ul>
+  </div>
   <a href="#" class="custom-btn">Quero levar minha marca!</a>
+  </nav>
 </header>
