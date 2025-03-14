de3e6f93e695d2574918c731c789710ae3899956
julio <juliorzomc@gmail.com>
2025-03-14 14:19:37 -0400

    fix section realization

--- resources/views/components/realization.blade.php
+++ resources/views/components/realization.blade.php
@@ -5,16 +5,16 @@
                 <h2 class="title-realization">Realização</h2>
                 <div class="logos">
                     <img src="{{ asset('img/logos-2.svg') }}" alt="Logo Amazontech">
-                    {{-- <img src="{{ asset('img/logo-sebrae.svg') }}" alt="Logo UFAM">
-                    <img src="{{ asset('img/image3.png') }}" alt="Logo UFAM">
-                    <img src="{{ asset('img/image2.png') }}" alt="Logo UFAM"> --}}
                 </div>
                 <div class="patrocinadores-realization d-flex flex-column align-items-center text-center">
-                    <h2 class="title-realization">Leve sua marca para o AmazonTech 2025!</h2>
-                    <p class="text-realization">Entenda como funciona para levar sua marca com o nosso <a
-                            class="link-realization" href="" target="_blank">plano de
-                            patrocínio.</a></p>
-                    <a href="#" class="btn btn-orange-realization" target="_blank">QUERO LEVAR MINHA MARCA</a>
+                    <h2 class="title-realization title-realization2">Leve sua marca para o AmazonTech 2025!</h2>
+                    <p class="text-realization"><strong>Entre em contato pelo e-mail</strong> <a class="link-realization"
+                            href="mailto:amazontech@rr.sebrae.com.br" target="_blank">amazontech@rr.sebrae.com.br</a> e
+                        descubra como participar.
+                        Entenda como funciona o nosso plano de patrocinio e faça parte dessa transformação.</p>
+                    <a href="{{ asset('img/AMAZONTEC.pdf') }}" class="btn btn-orange-realization"
+                        target="_blank">BAIXAR
+                        PLANO DE PATROCÍNIO</a>
                 </div>
             </div>
         </div>
